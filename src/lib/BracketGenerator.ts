51bf6863061a58ab86fe9f5ec119e9a25f47e2ea
Tom Fukushima <tomfukushima@gmail.com>
2026-03-26 09:57:18 -0600

    implement seeded algorithm

--- src/lib/BracketGenerator.ts
+++ src/lib/BracketGenerator.ts
@@ -9,6 +9,7 @@ class InitialBracketStructure {
     numParticipants: number;
     numberOfPairsInFirstRound: number; // number of pairs that will be in the first (potentially incomplete) round
     bracketStore: BracketStore;
+    seedPositions: number[] = [];
     
     constructor(bStore: BracketStore) {
         this.bracketStore = bStore;
@@ -33,6 +34,12 @@ class InitialBracketStructure {
             }
         }
 
+        if (this.bracketStore.isSeededMatch) {
+            this.buildSeedPositions();
+        } else {
+            this.seedPositions.length = 0; // clear seeded positions if not a seeded match
+        }
+
         //Now put in the participant names
         for (let i = 0; i < this.numParticipants; i++) {
             const pos = this.getPosition(i);
@@ -40,6 +47,46 @@ class InitialBracketStructure {
         }        
     }
 
+    buildSeedPositions() {
+        //create a list where the index is the participant index and the value is the position in the first round (0-indexed from the bottom).
+
+        // First create a list where the participant index is the position in the first round
+        const positions: number[] = [];
+        const size = this.bracketStore.brackets[this.bracketStore.rounds].length;
+        if (size === 0) return;
+
+        for (let i = 1; i < size / 4; i++) {
+            const even = i % 2 === 0;
+            positions.splice(0, 0, i * 2 + (even ? 1: 2));
+            positions.push(i * 2 + (even ? 2: 1));
+        }
+        positions.splice(0, 0, 1);
+        positions.push(2);
+
+        for (let i = positions.length - 1; i >= 0; i--) {
+            positions.splice(i, 0, size + 1 - positions[i]);
+        }
+
+        // special case for kata
+        if (this.bracketStore.isKata) {
+            // for Kata put the higher seeds last
+            const tmp: number[] = [];
+            const quarter = size / 4;
+            tmp.push(...positions.slice(0, quarter));
+            tmp.push(...positions.slice(quarter * 3));
+            tmp.push(...positions.slice(quarter * 2, quarter * 3));
+            tmp.push(...positions.slice(quarter, quarter * 2));
+            Object.assign(positions, tmp);
+        }
+
+        // now invert the list so that the index is the participant index and the value is the position in the first round
+        this.seedPositions.length = 0;
+        for (let i = 0; i < positions.length; i++) {
+            this.seedPositions[i] = positions.findIndex((v) => v === i + 1);
+        }
+
+    }
+  
     getPosition(participantIndex: number): Position {
         if (participantIndex < 0 || participantIndex >= this.numParticipants) {
             throw new Error("Participant index out of bounds");
@@ -48,16 +95,30 @@ class InitialBracketStructure {
         if (this.bracketStore.isKata && this.numParticipants <= 4) {
             // For kata with less than 4 participants, we put them all in the first round and leave some slots empty
             return { column: 0, row: participantIndex };
-        } else if (participantIndex < this.numberOfPairsInFirstRound * 2) {
-            // Put the participant in the first round
-            const column = this.bracketStore.rounds;
-            const row = participantIndex;
-            return { column, row };
+        } else if (this.bracketStore.isSeededMatch) {
+            if ((this.bracketStore.participants.length - participantIndex) <= (this.numberOfPairsInFirstRound * 2)) {
+                // Put the participant in the first round
+                const column = this.bracketStore.rounds;
+                const row = this.seedPositions[participantIndex];
+                return { column, row };
+            } else {
+                // Put the participant into the second round
+                const column = this.bracketStore.rounds - 1;
+                const row = Math.floor(this.seedPositions[participantIndex] / 2);
+                return { column, row };
+            }        
         } else {
-            // Put the participant into the second round
-            const column = this.bracketStore.rounds - 1;
-            const row = participantIndex - this.numberOfPairsInFirstRound;
-            return { column, row };
+            if (participantIndex < this.numberOfPairsInFirstRound * 2) {
+                // Put the participant in the first round
+                const column = this.bracketStore.rounds;
+                const row = participantIndex;
+                return { column, row };
+            } else {
+                // Put the participant into the second round
+                const column = this.bracketStore.rounds - 1;
+                const row = participantIndex - this.numberOfPairsInFirstRound;
+                return { column, row };
+            }
         }
     }
 }
